f2f81d1c7dd0deb264814be98d387ee018026100
itsjustshubh <86682116+itsjustshubh@users.noreply.github.com>
2023-12-27 18:39:31 +0530

    Fixed Nav Issues

---
+++ src/pages/Layout/Layout.js
@@ -0,0 +1,63 @@
+import React, { useState, useEffect } from 'react';
+import AnimatedCursor from 'react-animated-cursor';
+import { NavLink } from 'react-router-dom';
+import { styling } from "../Content"; // Ensure this path is correct
+import './Layout.css';
+
+function Layout({ children }) {
+  const cursorConfig = {
+      innerSize: 0.015 * window.innerHeight,
+      outerSize: 0.04 * window.innerHeight,
+      color: '256, 256, 256',
+      outerAlpha: 0.4,
+      innerScale: 0.5,
+      outerScale: 2,
+  };
+
+  function preloadImagesFromDirectory(directory) {
+      const images = directory.keys().map(directory);
+      images.forEach((imageSrc) => {
+        const img = new Image();
+        img.src = imageSrc;
+      });
+  }
+
+  function preloadVideo(videoSrc) {
+    const video = document.createElement('video');
+    video.src = videoSrc;
+    video.onloadeddata = () => {
+      console.log('Background video preloaded');
+    };
+  }
+
+  useEffect(() => {
+    // Preload images
+    const imagesDirectory = require.context('../../assets/images', false, /\.(png|jpe?g|svg)$/);
+    preloadImagesFromDirectory(imagesDirectory);
+
+    // Preload background video
+    preloadVideo(styling.background);
+  }, []);
+
+  return (
+    <div className="layout-background">
+      <AnimatedCursor {...cursorConfig} />
+      <video className="background-video" autoPlay muted loop playsInline>
+        <source src={styling.background} type="video/mp4"/>
+      </video>
+
+        <div className={`navbar ${!showNavbar ? 'navbar-hidden' : ''}`}>
+            <ul>
+                <li><NavLink to="/" className="nav-link">Home</NavLink></li>
+                <li><NavLink to="/education" className="nav-link">Education</NavLink></li>
+                <li><NavLink to="/projects" className="nav-link">Projects</NavLink></li>
+                <li><NavLink to="/contact" className="nav-link">Contact</NavLink></li>
+            </ul>
+        </div>
+
+        {children}
+    </div>
+  );
+}
+
+export default Layout;
---
+++ src/pages/NotFoundPage/NotFoundPage.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from 'react';
+import './NotFoundPage.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowCircleLeft } from "@fortawesome/free-solid-svg-icons";
+import { useNavigate } from 'react-router-dom';
+import { errorPage } from '../Content';
+
+const NotFoundPage = () => {
+    const navigate = useNavigate();
+    const [currentQuoteIndex, setCurrentQuoteIndex] = useState(0);
+    const [fadeOut, setFadeOut] = useState(false);
+
+    const goToPage = () => {
+        navigate(errorPage.button.link);
+    };
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setFadeOut(true); // Start fade out
+            setTimeout(() => {
+                setCurrentQuoteIndex(prevIndex =>
+                    (prevIndex + 1) % errorPage.quotes.length
+                );
+                setFadeOut(false); // Reset for fade in
+            }, 1000); // Change quote after fade out (1s duration)
+        }, 4000); // Change quote every 4 seconds (includes 1s for fade out)
+
+        return () => clearInterval(interval);
+    }, []);
+
+    const currentQuote = errorPage.quotes[currentQuoteIndex];
+
+    return (
+        <div className="page">
+            <h1 className="page__title">{errorPage.title}</h1>
+            <p className="page__subtitle">{errorPage.subtitle}</p>
+            <button className="page__button" onClick={goToPage}>
+                <FontAwesomeIcon icon={faArrowCircleLeft} /> {errorPage.button.text}
+            </button>
+            <div className={`quote-container ${fadeOut ? 'fade-out' : ''}`}>
+                <p className="page__text">{currentQuote.text}</p>
+                <p className="page__text__writer">~ {currentQuote.writer}</p>
+            </div>
+        </div>
+    );
+};
+
+export default NotFoundPage;
